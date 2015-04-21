7fa18ccff76c6234db364eda53d4a9b0cbd96dcb
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2015-04-22 00:30:35 +0200

    fix bug in nix expression

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ stdenv, haskellngPackages}:
+{ stdenv, haskellngPackages, SDL2_ttf}:
 
 let
   env=haskellngPackages.ghcWithPackages (p: with p; [
@@ -8,7 +8,7 @@ let
 in
   stdenv.mkDerivation {
     name = "sdl2-ttf";
-    buildInputs = [env];
+    buildInputs = [env SDL2_ttf];
     shellHook   = ''
       export NIX_GHC="${env}/bin/ghc"
       export NIX_GHCPKG="${env}/bin/ghc-pkg"
--- shell.nix
+++ shell.nix
@@ -3,4 +3,5 @@
 (import ./default.nix) {
   stdenv            = pkgs.stdenv;
   haskellngPackages = pkgs.haskellngPackages;
+  SDL2_ttf = pkgs.SDL2_ttf;
 }
